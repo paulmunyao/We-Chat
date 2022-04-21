7a720f67473f7087e1b2a4d1617764437bc6e418
Paul Munyao <paulmunyao094@gmail.com>
2022-04-21 23:52:04 +0300

    Add placeholder for user inputs

--- aigora/forms.py
+++ aigora/forms.py
@@ -5,14 +5,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-    widget = forms.TextInput(attrs={'class': 'form-control'})
+    widget = forms.TextInput(attrs={'placeholder': 'email','class': 'form-control'})
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
         widgets = {
-            'username':forms.TextInput(attrs={'class': 'form-control'}),
-            'email':forms.EmailInput(attrs={'class': 'form-control'}),
-            'password1':forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2':forms.PasswordInput(attrs={'class': 'form-control'}),
+            'username':forms.TextInput(attrs={'placeholder': 'username','class': 'form-control'}),
+            'email':forms.EmailInput(attrs={'placeholder': 'username','email': 'form-control'}),
+            'password1':forms.PasswordInput(attrs={'placeholder': 'password1','class': 'form-control'}),
+            'password2':forms.PasswordInput(attrs={'placeholder': 'password2','class': 'form-control'}),
         }
