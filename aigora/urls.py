10c8bafdd81ea2842332f2b7ca205b69c552f47f
Paul Munyao <paulmunyao094@gmail.com>
2022-04-21 22:13:57 +0300

    Create views path

--- aigora/urls.py
+++ aigora/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='home'),
     path('signup',views.signup,name='registration/signup'),
+    path('display',views.display,name='display'),
 ]
