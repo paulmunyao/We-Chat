05be0aa538948332aba8c10cc13b68ccee49d911
Paul Munyao <paulmunyao094@gmail.com>
2022-04-22 00:55:12 +0300

    Create a login path for user

--- aigora/forms.py
+++ aigora/forms.py
@@ -5,14 +5,14 @@ from django.forms import TextInput, EmailInput, PasswordInput
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    widget = forms.EmailInput(attrs={'placeholder': 'email','class': 'form-control'})
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'placeholder': 'email', 'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
         widgets = {
-            'username':forms.TextInput(attrs={'placeholder': 'username','class': 'form-control'}),
-            'password1':forms.PasswordInput(attrs={'placeholder': 'password1','class': 'form-control'}),
-            'password2':forms.PasswordInput(attrs={'placeholder': 'password2','class': 'form-control'}),
+            'username': forms.TextInput(attrs={'placeholder': 'username', 'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'password1', 'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'password2', 'class': 'form-control'}),
         }
