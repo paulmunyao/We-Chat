85401c6ac488fc7e6a43aa61191f817baf5c14d6
Paul Munyao <paulmunyao094@gmail.com>
2022-04-21 23:50:21 +0300

    Add closing tags to user input fields

--- aigora/forms.py
+++ aigora/forms.py
@@ -1,3 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    widget = forms.TextInput(attrs={'class': 'form-control'})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username':forms.TextInput(attrs={'class': 'form-control'}),
+            'email':forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1':forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2':forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
