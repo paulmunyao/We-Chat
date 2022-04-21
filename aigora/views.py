55e553802db76554052722308a10f7d47702f035
Paul Munyao <paulmunyao094@gmail.com>
2022-04-21 22:05:33 +0300

    Change the url path to home from index

--- aigora/views.py
+++ aigora/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'home.html')
 
 def signup(request):
     return render(request,'registration/signup.html')
