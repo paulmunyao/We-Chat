c16c726104ff2bf9587d2231572991e1d79b557c
Paul Munyao <paulmunyao094@gmail.com>
2022-04-22 08:32:30 +0300

    Install cloudinary dependancies

--- aigora/models.py
+++ aigora/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
