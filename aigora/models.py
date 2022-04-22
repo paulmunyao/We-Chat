c03a819057eef00c364d0d9043a06492bae29f9d
Paul Munyao <paulmunyao094@gmail.com>
2022-04-22 08:36:33 +0300

    Import models into views file

--- aigora/models.py
+++ aigora/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from .models import Profile
 
 # Create your models here.
 
--- aigora/views.py
+++ aigora/views.py
@@ -1,8 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm,EmailForm
+from .forms import UserRegisterForm, EmailForm, UpdateUserForm, UpdateProfileForm
 from django.core.mail import send_mail
 from django.conf import settings
 
@@ -34,8 +35,24 @@ def display(request):
             subject = "Send an email with Django"
             message = cd['message']
 
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [cd['recipient']])
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
+                      [cd['recipient']])
             messageSent = True
     else:
         form = EmailForm()
-    return render(request, 'display.html',{'form':form, 'messageSent':messageSent})
+    return render(request, 'display.html', {'form': form, 'messageSent': messageSent})
+
+
+@login_required(login_url='login')
+def profile(request):
+    if request.method == 'POST':
+        profile_form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(
+                request, ('Your profile was successfully updated!'))
+            return redirect('profile')
+    else:
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'profile_form': profile_form})
